a70fbc93c68e97014f73f54ac9b230e3763b4037
pedronicolas <pedro.nicolas.perez96@gmail.com>
2019-02-12 17:53:48 +0100

    added function to init of app

--- GttApiWeb/Services/ServicioCron.cs
+++ GttApiWeb/Services/ServicioCron.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.EntityFrameworkCore;
+using GttApiWeb.Models;
 
 namespace GttApiWeb.Services
 {
@@ -24,13 +25,26 @@ namespace GttApiWeb.Services
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Arrancando el servicio");
-            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromHours(12));
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
             return Task.CompletedTask;
         }
 
         public void DoWork(object state)
         {
-            _logger.LogInformation("Ejecutando tarea");
+            var optionsBuild = new DbContextOptionsBuilder<AppDBContext>();
+            optionsBuild.UseNpgsql("Host=192.168.99.100;Port=5432;Username=postgres;Password=example;Database=ApiGtt;");
+
+            using (var context = new AppDBContext(optionsBuild.Options))
+            {
+                context.Certificates.Load();
+                foreach (var cert in context.Certificates.Local)
+                {
+                    //DateTime maxDateTime = DateTime();
+                    _logger.LogInformation(cert.alias);
+                }
+
+            }
+            
         }
 
 
